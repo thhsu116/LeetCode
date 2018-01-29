6699f24c619dd881d26191cd22cb6d86c337e237
thhsu116 <35554384+thhsu116@users.noreply.github.com>
2018-01-29 00:17:21 -0800

    Create 145-Binary_Tree_Postorder Traversal_iter_med.py

---
+++ BinaryTree/145-Binary_Tree_Postorder Traversal_iter_med.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ret = []
+        stack = [(root, False)]  # use a flag to indicate whether the node has been visited or not.
+        while stack:
+            node, visited = stack.pop()
+            if not node:
+                continue
+            if visited:
+                ret.append(node.val)
+            else:
+                stack.append((node, True))
+                stack.append((node.right, False))
+                stack.append((node.left, False))
+        return ret
