9b10186a6e451467ed894786dff6dd49c1a14279
thhsu116 <35554384+thhsu116@users.noreply.github.com>
2018-01-29 22:52:59 -0800

    Update 94-Binary_Tree_Inorder_Traversal_iter_med.py

--- BinaryTree/94-Binary_Tree_Inorder_Traversal_iter_med.py
+++ BinaryTree/94-Binary_Tree_Inorder_Traversal_iter_med.py
@@ -4,7 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+# https://en.wikipedia.org/wiki/Tree_traversal#In-order
 class Solution:
     def inorderTraversal(self, root):
         """
