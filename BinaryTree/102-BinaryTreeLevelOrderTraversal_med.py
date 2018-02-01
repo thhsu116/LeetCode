fdadff1486bed57c64fd322a2c2851b3d309e3c2
thhsu116 <35554384+thhsu116@users.noreply.github.com>
2018-01-31 23:04:47 -0800

    Create 102-BinaryTreeLevelOrderTraversal_med.py

---
+++ BinaryTree/102-BinaryTreeLevelOrderTraversal_med.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# BFS
+# inpput can be []!!!
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        queue = [root]
+        ret = []
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+            if level:
+                ret.append(level)
+        return ret
