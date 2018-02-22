de3746dae596d70429b3a1ff69848c492c90d734
thhsu116 <35554384+thhsu116@users.noreply.github.com>
2018-02-21 22:20:22 -0800

    Create 102-BinaryTreeLevelOrderTraversal_hard.py

---
+++ BinaryTree/102-BinaryTreeLevelOrderTraversal_hard.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# recursive
+class Codec:
+
+    def serialize(self, root):
+        def doit(node):
+            if node:
+                vals.append(str(node.val))
+                doit(node.left)
+                doit(node.right)
+            else:
+                vals.append('#')
+        vals = []
+        doit(root)
+        return ' '.join(vals)
+
+    def deserialize(self, data):
+        def doit():
+            val = next(vals)
+            if val == '#':
+                return None
+            node = TreeNode(int(val))
+            node.left = doit()
+            node.right = doit()
+            return node
+        vals = iter(data.split())
+        return doit()
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
