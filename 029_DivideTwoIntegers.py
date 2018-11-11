da292be5566e65d3497566dfc7f30812b9775707
thhsu116 <35554384+thhsu116@users.noreply.github.com>
2018-11-11 00:33:21 -0800

    Update 029_DivideTwoIntegers.py

--- 029_DivideTwoIntegers.py
+++ 029_DivideTwoIntegers.py
@@ -98,3 +98,22 @@ class Solution:
             dividend -= divisor << i
             q += (1 << i)
         return q if (s_dividend>0) == (s_divisor>0) else -q
+# https://leetcode.com/problems/divide-two-integers/discuss/179759/Python-7-lines-(beats-99):-How-you-learned-to-divide-when-you-were-7-years-old
+def divide(self, dividend, divisor):
+
+    # this is necessary; otherwise phase 1 never terminates
+    if dividend == 0: return 0
+
+    # initialize
+    i, result, p, q = map(abs, (0, 0, dividend, divisor))
+    
+    # phase 1
+    while q << i <= p: i += 1
+
+    # phase 2
+    for j in reversed(range(i)):
+        if q << j <= p: p, result = p - (q << j), result + (1 << j)
+
+    # stupid leetcode restrictions
+    if (dividend > 0) != (divisor > 0) or result < -1 << 31: result = -result
+    return min(result, (1 << 31) - 1)
