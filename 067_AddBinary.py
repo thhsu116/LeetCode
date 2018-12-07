24bb72f787f622ece8a9c0ddebf06a7566afa24f
thhsu116 <35554384+thhsu116@users.noreply.github.com>
2018-12-06 23:38:29 -0800

    Update 067_AddBinary.py

--- 067_AddBinary.py
+++ 067_AddBinary.py
@@ -26,3 +26,8 @@ class Solution:
             return '1' + ret
         else:
             return ret
+
+# python one liner
+class Solution:
+    def addBinary(self, a, b):
+        return bin(eval('0b' + a) + eval('0b' + b))[2:]
