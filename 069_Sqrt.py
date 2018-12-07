281f0e73bbe7cad7e892352a14c63899b375b350
thhsu116 <35554384+thhsu116@users.noreply.github.com>
2018-12-07 09:58:09 -0800

    Update 069_Sqrt.py

--- 069_Sqrt.py
+++ 069_Sqrt.py
@@ -24,3 +24,22 @@ class Solution:
                 r = c
 
 # Newton's method
+# https://en.wikipedia.org/wiki/Newton%27s_method
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 1:
+            return 1
+        elif x == 0:
+            return 0
+        
+        last = 0
+        cur = 1
+        while True:
+            last = cur
+            cur = (cur + x/cur)/2
+            if int(last) == int(cur):
+                return int(last)
